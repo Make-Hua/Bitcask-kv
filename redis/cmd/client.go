49deeda5932e10f5c19ff3ffb20f928f4ac7d0ce
Make-Hua <2262829340@QQ.com>
2025-03-23 20:32:26 +0800

    updata redis

--- redis/cmd/client.go
+++ redis/cmd/client.go
@@ -17,12 +17,31 @@ func newWrongNumberOfArgsError(cmd string) error {
 type cmdHandler func(cli *BitcaskClient, args [][]byte) (interface{}, error)
 
 var supportedCommands = map[string]cmdHandler{
-	"set":   set,
-	"get":   get,
-	"hset":  hset,
-	"sadd":  sadd,
+
+	/* string */
+	"set": set,
+	"get": get,
+	"del": del,
+
+	/* hash */
+	"hset": hset,
+	"hget": hget,
+	"hdel": hdel,
+
+	/* set */
+	"sadd":      sadd,
+	"sismember": sismember,
+	"srem":      srem,
+
+	/* list */
 	"lpush": lpush,
-	"zadd":  zadd,
+	"rpush": rpush,
+	"lpop":  lpop,
+	"rpop":  rpop,
+
+	/* zset */
+	"zadd":   zadd,
+	"zscore": zscore,
 }
 
 type BitcaskClient struct {
@@ -82,7 +101,7 @@ func get(cli *BitcaskClient, args [][]byte) (interface{}, error) {
 		return nil, newWrongNumberOfArgsError("get")
 	}
 
-	// 取出参数 set key1 value1
+	// 取出参数 get key
 	value, err := cli.db.Get(args[0])
 	if err != nil {
 		return nil, err
@@ -91,8 +110,37 @@ func get(cli *BitcaskClient, args [][]byte) (interface{}, error) {
 	return value, nil
 }
 
+func del(cli *BitcaskClient, args [][]byte) (interface{}, error) {
+
+	if len(args) != 1 {
+		return nil, newWrongNumberOfArgsError("del")
+	}
+
+	// 由于 Del 是直接调用 Delete 接口的，所以需要先判断是否存在该数据
+	// 先判断该 key 是否存在
+	var ok = 0
+	key := args[0]
+	_, err := cli.db.Get(key)
+	if err != nil {
+		if err == bitcaskkv.ErrKeyNotFound {
+			return redcon.SimpleInt(ok), nil
+		}
+		return nil, err
+	}
+
+	// 取出参数 set key1 value1
+	err = cli.db.Del(key)
+	if err != nil {
+		return nil, err
+	}
+	ok = 1
+
+	return redcon.SimpleInt(ok), nil
+}
+
 func hset(cli *BitcaskClient, args [][]byte) (interface{}, error) {
 
+	// HSET key field value
 	if len(args) != 3 {
 		return nil, newWrongNumberOfArgsError("hset")
 	}
@@ -109,8 +157,47 @@ func hset(cli *BitcaskClient, args [][]byte) (interface{}, error) {
 	return redcon.SimpleInt(ok), nil
 }
 
+func hget(cli *BitcaskClient, args [][]byte) (interface{}, error) {
+
+	// HGET key field
+	if len(args) != 2 {
+		return nil, newWrongNumberOfArgsError("hget")
+	}
+
+	key, field := args[0], args[1]
+	value, err := cli.db.HGet(key, field)
+	if err != nil {
+		return nil, err
+	}
+
+	return value, nil
+}
+
+func hdel(cli *BitcaskClient, args [][]byte) (interface{}, error) {
+
+	// HDEL key field
+	if len(args) != 2 {
+		return nil, newWrongNumberOfArgsError("hdel")
+	}
+
+	key, field := args[0], args[1]
+	ok, err := cli.db.HDel(key, field)
+	if err != nil {
+		return nil, err
+	}
+
+	// 数据不存在
+	if !ok {
+		return redcon.SimpleInt(0), nil
+	}
+
+	return redcon.SimpleInt(1), nil
+}
+
 func sadd(cli *BitcaskClient, args [][]byte) (interface{}, error) {
 
+	// 往集合key中存入元素，元素存在则忽略，若key不存在则新建
+	// SADD key member
 	if len(args) != 2 {
 		return nil, newWrongNumberOfArgsError("sadd")
 	}
@@ -127,8 +214,51 @@ func sadd(cli *BitcaskClient, args [][]byte) (interface{}, error) {
 	return redcon.SimpleInt(ok), nil
 }
 
+func sismember(cli *BitcaskClient, args [][]byte) (interface{}, error) {
+
+	// 判断 member元素是否存在于集合key中
+	// SISMEMBER key member
+	if len(args) != 2 {
+		return nil, newWrongNumberOfArgsError("sismember")
+	}
+
+	var ok = 0
+	key, member := args[0], args[1]
+	res, err := cli.db.SIsMember(key, member)
+	if err != nil {
+		return nil, err
+	}
+	if res {
+		ok = 1
+	}
+
+	return redcon.SimpleInt(ok), nil
+}
+
+func srem(cli *BitcaskClient, args [][]byte) (interface{}, error) {
+
+	// 从集合key中删除元素
+	// SREM key member
+	if len(args) != 2 {
+		return nil, newWrongNumberOfArgsError("srem")
+	}
+
+	var ok = 0
+	key, member := args[0], args[1]
+	res, err := cli.db.SRem(key, member)
+	if err != nil {
+		return nil, err
+	}
+	if res {
+		ok = 1
+	}
+
+	return redcon.SimpleInt(ok), nil
+}
+
 func lpush(cli *BitcaskClient, args [][]byte) (interface{}, error) {
 
+	// LRUSH key value
 	if len(args) != 2 {
 		return nil, newWrongNumberOfArgsError("lpush")
 	}
@@ -140,6 +270,54 @@ func lpush(cli *BitcaskClient, args [][]byte) (interface{}, error) {
 	}
 
 	return redcon.SimpleInt(res), nil
+}
+
+func rpush(cli *BitcaskClient, args [][]byte) (interface{}, error) {
+
+	// RRUSH key value
+	if len(args) != 2 {
+		return nil, newWrongNumberOfArgsError("rpush")
+	}
+
+	key, value := args[0], args[1]
+	res, err := cli.db.RPush(key, value)
+	if err != nil {
+		return nil, err
+	}
+
+	return redcon.SimpleInt(res), nil
+}
+
+func lpop(cli *BitcaskClient, args [][]byte) (interface{}, error) {
+
+	// LPOP key
+	if len(args) != 1 {
+		return nil, newWrongNumberOfArgsError("lpop")
+	}
+
+	key := args[0]
+	res, err := cli.db.LPop(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func rpop(cli *BitcaskClient, args [][]byte) (interface{}, error) {
+
+	// LPOP key
+	if len(args) != 1 {
+		return nil, newWrongNumberOfArgsError("rpop")
+	}
+
+	key := args[0]
+	res, err := cli.db.RPop(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
 
 func zadd(cli *BitcaskClient, args [][]byte) (interface{}, error) {
@@ -159,4 +337,20 @@ func zadd(cli *BitcaskClient, args [][]byte) (interface{}, error) {
 	}
 
 	return redcon.SimpleInt(ok), nil
+}
+
+func zscore(cli *BitcaskClient, args [][]byte) (interface{}, error) {
+
+	// ZSCORE key member
+	if len(args) != 2 {
+		return nil, newWrongNumberOfArgsError("zscore")
+	}
+
+	key, member := args[0], args[1]
+	score, err := cli.db.ZScore(key, member)
+	if err != nil {
+		return nil, err
+	}
+
+	return score, nil
 }
